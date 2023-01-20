767370292096dba83c22c57ea2cd28fcb4022427
tokhtaraubakirov <student.aubakirov@dku.kz>
2023-01-20 19:21:53 +0600

    second commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let table = document.getElementById("age-table");
+table.getElementsByTagName("label");
+table.querySelectorAll("td")[0];
+let form = document.getElementsByName("search")[0];
+form.querySelector("input");
+let allInputs = document.querySelectorAll("input");
+allInputs[allInputs.length - 1]; 
+
+
+{/* <script>
+  let body = document.body;
+
+  body.innerHTML = "<!--" + body.tagName + "-->";
+
+  alert( body.firstChild.data ); // что выведет?
+</script> */}
+
+//Вывод alert("BODY");
+
